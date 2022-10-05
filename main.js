a1bcca03d5e30599be2d4490f8c15edaa1e271d4
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-05 15:58:28 +0200

    Content zum dialog-Element erstellt, gestyled und js-Verarbeitung implementiert

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', dcl => {
     console.log('Document is ready ...');
+
+    // eventListener f. demoDialogClose
+    document.querySelector('#demoDialogClose').addEventListener('click', e => {
+        document.querySelector('#demoDialog').close();
+    });
+
+
+    const demoDialogModal = document.querySelector('#demoModalDialog');
+
+    // eventListener f. openModalDialog
+    document.querySelector('#openModalDialog').addEventListener('click', e => {
+        demoDialogModal.showModal();
+    });
+
+    // eventListener f. demoDialogCloseModal
+    document.querySelector('#demoDialogCloseModal').addEventListener('click', e => {
+        if(document.querySelector('#wantRealyClose').checked) {
+            demoDialogModal.close();
+        }
+    });
 });
